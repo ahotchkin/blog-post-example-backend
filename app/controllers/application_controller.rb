5885f34fd46f83d7eb9c93eea282d578f4d5daaa
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-08 16:12:40 -0500

    Uncomment code alerting ApplicationController that cookies are being used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::API
-  # *********************************************************************************************
-  # I DIDN'T METION THIS IN MY BLOG. GO BACK AND EDIT AND IS IT REALLY EEVEN NEEDED?
-  # include ::ActionController::Cookies
+  include ::ActionController::Cookies
 
   def current_user
     current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
