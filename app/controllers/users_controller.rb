f134e476bbe4713c3cc35bf273876d79d921da3b
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-08 16:13:09 -0500

    Add create method and private method for user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      render json: user
+    else
+      resp = {
+        :error => @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+
 end
