f05de67bc47b2b467dae170ed29ef1db16e9c57d
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-15 14:40:24 -0500

    Add migration to add first_name and last_name columns to Users table; run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_170100) do
+ActiveRecord::Schema.define(version: 2021_02_15_191121) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
