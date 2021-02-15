65ab6008152817ff48e3af923017009f22c885c0
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-15 14:39:58 -0500

    Add first_name and last_name to list of attributes; add full_name custom attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :username
+  attributes :username, :first_name, :last_name
+
+  attribute :full_name do |object|
+    "#{object.first_name} #{object.last_name}"
+  end
 end
